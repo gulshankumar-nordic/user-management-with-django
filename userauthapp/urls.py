57bc6901b5756d09ef49a2dfe784cbb363fc8620
Gulshan <honeykulta@gmail.com>
2020-02-13 00:30:24 +0200

    Added new urls

--- userauthapp/urls.py
+++ userauthapp/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('login/', views.loginUser, name='login'),
     path('logout/', views.logoutUser, name='logout'),
-    path('register/', views.registerUser, name='register')
+    path('register/', views.registerUser, name='register'),
+    path('edit-profile/', views.editProfile, name='edit-profile'),
+    path('change-password/', views.changePassword, name='change-password')
 ]
